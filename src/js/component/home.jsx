788cd564e823c5e92a3da771b5752c24eaae5212
Víctor Bravo <vbravo.quintana@gmail.com>
2024-08-29 13:44:42 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import '../../styles/index.css'
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+function home() {
+	const [tasks, setTasks] = useState([]);
+	const [inputValue, setInputValue] = useState('');
+
+	const handleKeyUp = (e) => {
+		if (e.key === 'Enter' && inputValue.trim()) {
+			setTasks([...tasks, inputValue]);
+			setInputValue('');
+		}
+	};
+
+	const handleDelete = (index) => {
+		setTasks(tasks.filter((_, i) => i !== index));
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="todo-container">
+			<h1 className="title">todos</h1>
+			<div className="todo-list">
+				<input
+					type="text"
+					placeholder="What needs to be done?"
+					className="todo-input"
+					value={inputValue}
+					onChange={(e) => setInputValue(e.target.value)}
+					onKeyUp={handleKeyUp}
+				/>
+				<ul className="todo-items">
+					{tasks.length === 0 ? (
+						<li className="no-tasks">No hay tareas, añadir tareas</li>
+					) : (
+						tasks.map((task, index) => (
+							<li
+								key={index}
+								className="todo-item"
+								onMouseEnter={(e) => e.currentTarget.classList.add('hover')}
+								onMouseLeave={(e) => e.currentTarget.classList.remove('hover')}
+							>
+								{task}
+								<button className="delete-btn" onClick={() => handleDelete(index)}>
+									X
+								</button>
+							</li>
+						))
+					)}
+				</ul>
+				<div className="items-left">{tasks.length} {tasks.length === 1 ? 'task' : 'tasks'} left</div>
+			</div>
 		</div>
 	);
-};
+}
 
-export default Home;
+export default home;
